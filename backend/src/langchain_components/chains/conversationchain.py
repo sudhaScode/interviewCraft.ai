98191afbfaf602803b990d3b84878da87a31019b
Sudharshan Bommasani <sudharshan.bommasani@capgemini.com>
2024-08-09 02:11:38 +0530

    sperated backend

--- backend/src/langchain_components/chains/conversationchain.py
+++
@@ -1,5 +0,0 @@
-from langchain.chains.conversation.base import ConversationChain
-
-def setup_conversationchain (llm ,memory, prompt):
-    chain =  ConversationChain(llm=llm, memory=memory, prompt=prompt, verbose=True)
-    return chain
--- backend/src/langchain_components/chains/simplesequentialchain.py
+++
@@ -1,7 +0,0 @@
-from langchain.chains import SimpleSequentialChain
-
-
-
-def setup_simple_chain(chains):
-    simple_chain = SimpleSequentialChain({chains: chains,  "verbose":True})
-    return simple_chain

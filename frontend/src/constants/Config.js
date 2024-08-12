4c44cc6ee559db89455cb72a26a0c65942401fea
Sudharshan Bommasani <sudharshan.bommasani@capgemini.com>
2024-08-12 18:40:36 +0530

    secure server point

--- frontend/src/constants/Config.js
+++ frontend/src/constants/Config.js
@@ -1,2 +1,2 @@
-export const URL_ENDPOINT = "https://https://interviewcraftai-gyf7cwdshbb2cfdj.southcentralus-01.azurewebsites.net/:8000"
+export const URL_ENDPOINT = "https://interviewcraftai-gyf7cwdshbb2cfdj.southcentralus-01.azurewebsites.net/:8000"
 // export const URL_ENDPOINT = "http://127.0.0.1:8000"
